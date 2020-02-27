8fc4f87f2271daf25abb773c6562f03e7ff725ab
Laig Gasanov <laig.gasanov@gmail.com>
2020-02-27 16:23:33 +0700

    refactoring(Main.h): исправлено оформление по TODO

--- lab1/Main.h
+++ lab1/Main.h
@@ -4,21 +4,22 @@
 #include "Pointers.h"
 #include "DynamicMemory.h"
 
-
 enum Commands
 {
-	ARRAYS, //TODO: Заглавная буква большая, остальные маленькие
-	FUNCTIONS, //TODO: тоже самое
-	POINTERS, //TODO: тоже самое
-	DYNAMIC_MEMORY, //TODO: тоже самое, без нижнего подчеркивания, каждое новое слово с заглавной
-	EXIT //TODO: тоже самое
+	Arrays, //TODO: Заглавная буква большая, остальные маленькие
+	Functions, //TODO: тоже самое
+	Pointers, //TODO: тоже самое
+	DynamicMemory, //TODO: тоже самое, без нижнего подчеркивания, каждое новое слово с заглавной
+	Exit //TODO: тоже самое
 };
 
 //TODO: имхо, эту функцию надо было в main.cpp поместить. К чему она здесь одна болтается?
+//Это Main.h, в Main.cpp она тоже ни к чему
 void PrintMainMenu()
 {
 	std::cout << "\n\t0) Массивы\n"
 		"\t1) Функции\n"
 		"\t2) Указатели и ссылки\n"
 		"\t3) Динамическая память\n";
+		"\t4) Выход из программы\n";
 }
