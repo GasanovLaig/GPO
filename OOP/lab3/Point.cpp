64bb998a3ed5f950fa7b9156723916e195538fbf
Gasanov Laig <laig.gasanov@gmail.com>
2020-04-27 16:09:01 +0700

    feature(lab3/): реализована структура Point

---
+++ OOP/lab3/Point.cpp
@@ -0,0 +1,17 @@
+#include "Point.h"
+
+void Point::MakePoint(float x, float y)
+{
+	X = x;
+	Y = y;
+}
+
+void Point::SetX(float x)
+{
+	X = x;
+}
+
+void Point::SetY(float y)
+{
+	Y = y;
+}
---
+++ OOP/lab3/Point.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct Point
+{
+	float X;
+	float Y;
+
+	void MakePoint(float x, float y);
+
+	void SetX(float x);
+	void SetY(float y);
+};
